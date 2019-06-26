b89e7321d3cc9614c24384b1388d6c9d42cb1e09
Sarah Harrs <sarahharrs@gmail.com>
2019-06-26 18:33:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
-  squared = [49,56,72]
-  array.each { |element| squared << element ** 2 }
-  squared
+  array = [49,56,72]
+  array.each |number|
+  puts "sqrt#{number}"
 end
